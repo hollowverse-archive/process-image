6c4d42c6cb4a0bc188d103aeb20f3beb2793039f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-03-28 18:24:39 +0200

    Switch to using cloudinary

--- src/processImage.ts
+++ src/processImage.ts
@@ -1,13 +1,18 @@
 import awsSdk from 'aws-sdk';
-import { cropImage } from './helpers';
 import { Handler, S3Event } from 'aws-lambda'; // tslint:disable-line:no-implicit-dependencies
-
-const rekognition = new awsSdk.Rekognition({
-  region: 'us-east-1',
-});
+import cloudinary from 'cloudinary';
+import path from 'path';
+import fetch from 'node-fetch';
+import fs from 'fs';
 
 const s3 = new awsSdk.S3({
   region: 'us-east-1',
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // tslint:disable-next-line:max-func-body-length
@@ -31,40 +36,46 @@ export const processImage: Handler<S3Event> = async (event, _context, done) => {
         })
         .promise();
     } else if (eventName.startsWith('ObjectCreated:')) {
-      const rekognitionResponse = await rekognition
-        .detectFaces({
-          Image: {
-            S3Object: {
-              Bucket: sourceBucketName,
-              Name: sourceObjectKey,
-            },
-          },
+      const { Body } = await s3
+        .getObject({
+          Bucket: sourceBucketName,
+          Key: sourceObjectKey,
         })
         .promise();
 
-      if (
-        rekognitionResponse.FaceDetails !== undefined &&
-        rekognitionResponse.FaceDetails.length > 0
-      ) {
-        const { Body } = await s3
-          .getObject({
-            Bucket: sourceBucketName,
-            Key: sourceObjectKey,
-          })
-          .promise();
+      const extensionName = path.extname(sourceObjectKey);
 
-        const buffer = await cropImage({
-          rekognitionResponse,
-          body: Body as Buffer,
-        });
+      const image = `data:image/${extensionName
+        .split('.')
+        .pop()};base64,${(Body as Buffer).toString('base64')}`;
 
+      const { eager: [{ url }] } = await new Promise<any>(resolve =>
+        cloudinary.uploader.upload(image, resolve, {
+          eager: [
+            {
+              width: 500,
+              height: 500,
+              crop: 'thumb',
+              gravity: 'faces',
+              // format: 'jpg',
+            },
+          ],
+        }),
+      );
+
+      const buffer = await (await fetch(url)).buffer();
+      const targetObjectKey = sourceObjectKey;
+
+      if (process.env.NODE === 'production') {
         await s3
           .putObject({
-            Key: sourceObjectKey,
+            Key: targetObjectKey,
             Bucket: targetBucketName,
             Body: buffer,
           })
           .promise();
+      } else {
+        fs.writeFileSync(path.basename(targetObjectKey), buffer);
       }
     } else {
       throw new TypeError(`Unexpected event name: ${eventName}`);
