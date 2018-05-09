ea02cbce26ed3ea328aee502f4a2964f374c03bc
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-05-09 19:29:18 +0300

    Migrate to AWS Secrets Manager (#12)

---
+++ clownOverrides/clownCallback.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { omit } = require('lodash');
+
+module.exports = clownFs => {
+  clownFs.editJson('package.json', json => omit(json, 'jest'));
+};
